d110cea66b7d87dc81caa35cf8cb281816839a01
Viswa Kodela <viswakodela@gmail.com>
2019-06-15 10:06:29 -0400

    Initial Commit

---
+++ CoreML SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CoreML SwiftUI
+//
+//  Created by Viswa Kodela on 6/15/19.
+//  Copyright © 2019 Viswa Kodela. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
