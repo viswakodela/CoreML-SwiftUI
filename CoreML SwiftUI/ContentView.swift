da76d0439a9258c72a001d441cdeefca4fd71996
Viswa Kodela <viswakodela@gmail.com>
2019-06-15 10:39:52 -0400

    Implemented the User Interface part

--- CoreML SwiftUI/ContentView.swift
+++ CoreML SwiftUI/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    @State private var wakeUp = Date()
+    @State private var sleepAmount: Double = 8
+    @State private var coffeeAmount: Int = 1
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                    .lineLimit(nil) //for getting multiple lines
+                
+                DatePicker($wakeUp, displayedComponents: .hourAndMinute)
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                    .lineLimit(nil)
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }.padding(.bottom)
+                
+                Text("Daily Coffee intake")
+                    .font(.headline)
+                    .lineLimit(nil)
+                Stepper(value: $coffeeAmount, in: 1...20, step: 1) {
+                    if coffeeAmount == 1 {
+                        Text("1 cup")
+                    } else {
+                        Text("\(coffeeAmount) cups")
+                    }
+                }.padding(.bottom)
+                
+                 Spacer()
+            }
+                .navigationBarItems(trailing:
+                    Button(action: {
+                        //Code here for calculate
+                    }, label: {
+                        Text("Calculate")
+                            .font(.headline)
+                    }))
+                .navigationBarTitle(Text("Better Best"))
+                .padding()
+        }
     }
 }
 
